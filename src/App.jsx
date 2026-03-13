4a7304de8290a0ff6e8d5338c97f7968fadb609a
shivendra0712 <patelshivendra817@gmail.com>
2026-03-13 17:14:46 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import axios from 'axios';
+import React, { useState } from 'react'
+
+
+const App = () => {
+  const [bgColor, setBgColor] = useState('white');
+
+  const handleButtonClick = () => {
+    setBgColor('black');
+  };
+
+  return (
+    <div  className='container' style={{ backgroundColor: bgColor, height: '100vh', width: '100vw', margin: 0, padding: 0 }}>
+      <button onClick={handleButtonClick}>Click Me</button>
+    </div>
+  )
+}
+
+export default App
